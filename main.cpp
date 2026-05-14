d9de12b60525e2aa50f47dfc94dfae1640c0d8bd
geng hongyu <1013367188@qq.com>
2026-05-14 09:33:54 +0900

    vection version-final.

--- main.cpp
+++ main.cpp
@@ -10,15 +10,28 @@ public:
         m_container.reserve(m_size);
     }
 
-    // rule of five 先不考虑
 
-    // 获取key对应的value，如果不存在，返回-1
-    int get(int key) const {
-        for (const auto& item : m_container) {
-            if (item.first == key) {
-                return item.second;
+    /*
+        获取key对应的value。
+        没找到，返回-1.
+        找到，返回value，并将元素移动到vector尾部，表示最新使用
+    */
+    int get(int key) {
+        for (int i = 0; i < m_container.size(); i++) {
+            if (m_container[i].first == key) {
+                // 找到key
+                auto tmp_item = m_container[i];
+
+                // 删除元素
+                m_container.erase(m_container.begin() + i);
+
+                // 尾部追加元素
+                m_container.push_back(tmp_item);
+
+                return tmp_item.second;
             }
         }
+
         return -1;
     }
 
@@ -43,12 +56,18 @@ public:
         m_container.push_back({key, val});
     }
 
+    ///////////////////////////////////////////////
+    void showinfo() const {
+        cout << "size=" << m_container.size() << " capacity=" << m_container.capacity() << endl;
+        for (const auto& item : m_container) {
+            cout << "key=" << item.first << " value=" << item.second << endl;
+        }
+    }
+
 private:
     int m_size = 0;
     vector<pair<int, int>> m_container;
 };
-
-
 
 
 int main()
@@ -56,17 +75,14 @@ int main()
     CLRUCache cache(5);
 
     // 追加元素
-    cache.put(1, 34);
-    cache.put(2, 56);
-    cache.put(5, 57);
-    cache.put(6, 193);
-    cache.put(3, 23);
-    
-    // 打印元素
-    cout << cache.get(1) << endl;
-    cout << cache.get(10) << endl;
-    
+    cache.put(1, 23);
+    cache.put(5, 12);
+    cache.put(12, 6);
+    cache.showinfo();
 
+    // 获取元素
+    cout << cache.get(5) << endl;
+    cache.showinfo();
 
 
 
